1f948a898e2426341b310b6dba8139113f6278c0
akclifto <akclifto@asu.edu>
2020-09-06 12:51:03 -0700

    update js function into proper methods:

--- PortfolioWeb/PortfolioWeb/wwwroot/js/site.js
+++ PortfolioWeb/PortfolioWeb/wwwroot/js/site.js
@@ -12,32 +12,40 @@ function myfunction() {
  * expands and collapes home screen section panels on click.
  */
 var panel = document.getElementsByClassName("panel");
+var eduPanel = document.getElementsByClassName("edu-panel");
 
-for (var i = 0; i < panel.length; i++) {
-    panel[i].addEventListener("click", function () {
+//for (var i = 0; i < panel.length; i++) {
+//    panel[i].addEventListener("click", function () {
+//        this.classList.toggle("active");
+//        var content = this.nextElementSibling;
+//        if (content.style.maxHeight) {
+//            content.style.maxHeight = null;
+//        } else {
+//            content.style.maxHeight = content.scrollHeight + "px";
+//        }
+//    });
+//}
+
+var panelName = document.getElementsByClassName("panel");
+prepPanel(panelName);
+var eduPanel = document.getElementsByClassName("eduPanel");
+
+function prepPanel(panelName) {
+    for (var i = 0; i < panelName.length; i++) {
+        panelName[i].addEventListener('click', animatePanel);
+    }
+}
+
+function animatePanel() {
+
         this.classList.toggle("active");
-        var content = this.nextElementSibling;
+    var content = this.nextElementSibling;
         if (content.style.maxHeight) {
             content.style.maxHeight = null;
         } else {
             content.style.maxHeight = content.scrollHeight + "px";
         }
-    });
 }
 
-
-
-function animatePanel(panelName) {
-
-    var panel = document.getElementsByClassName("panel");
-
-    panelName.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight) {
-        content.style.maxHeight = null;
-    } else {
-        content.style.maxHeight = content.scrollHeight + "px";
-    }
-}
 
 
