7bf4e89bda9b59ad9ff76f50ff76bde36e975393
akclifto <akclifto@asu.edu>
2020-09-06 11:57:27 -0700

    jadd js for animated home screen panels

--- PortfolioWeb/PortfolioWeb/wwwroot/js/site.js
+++ PortfolioWeb/PortfolioWeb/wwwroot/js/site.js
@@ -2,4 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
-    //Script to collapse and open Home page sections.
+    //test script
+function myfunction() {
+    alert("Helloo!");
+}
+
+/**
+ * Animated Collapsible panels for Home page, slide down.
+ */
+var coll = document.getElementsByClassName("panel");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+}
