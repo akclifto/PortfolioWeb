34181c5811660cf2fb8c9a95100ba724f2da84f6
akclifto <akclifto@asu.edu>
2020-09-09 17:58:24 -0700

    json service partial

--- PortfolioWeb/PortfolioWeb/Pages/Index.cshtml.cs
+++ PortfolioWeb/PortfolioWeb/Pages/Index.cshtml.cs
@@ -1,20 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using PortfolioWeb.Model;
+using PortfolioWeb.Services;
+using System.Collections.Generic;
 
 namespace PortfolioWeb.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        //create a JsonFileSectionService object
+        public JsonFileSectionService SectionService;
+        //put the object from the json file in a list.
+        public IEnumerable<Section> SectionList { get; private set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(
+            ILogger<IndexModel> logger,
+            JsonFileSectionService sectionService)
         {
             _logger = logger;
+            SectionService = sectionService;
         }
 
         public void OnGet()
         {
+            SectionList = SectionService.GetSections();
         }
     }
 }
