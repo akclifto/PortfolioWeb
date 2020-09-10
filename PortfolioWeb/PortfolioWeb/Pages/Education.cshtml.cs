86d2f6aa2a2d22383cc9673d47b7fda847d0acc8
akclifto <akclifto@asu.edu>
2020-09-10 12:12:42 -0700

    convert experience tab to cs and json impl

--- PortfolioWeb/PortfolioWeb/Pages/Education.cshtml.cs
+++ PortfolioWeb/PortfolioWeb/Pages/Education.cshtml.cs
@@ -1,11 +1,23 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PortfolioWeb.Model;
+using PortfolioWeb.Services;
+using System.Collections.Generic;
 
 namespace PortfolioWeb.Pages
 {
-    public class Index1Model : PageModel
+    public class EducationModel : PageModel
     {
+        public JsonFileSectionService SectionService;
+
+        public IEnumerable<Section> SectionList{get; private set;}
+
+        public EducationModel(JsonFileSectionService sectionService)
+        {
+            SectionService = sectionService;
+        }
         public void OnGet()
         {
+            SectionList = SectionService.GetSections();
         }
     }
 }
--- PortfolioWeb/PortfolioWeb/Pages/Professional.cshtml.cs
+++ PortfolioWeb/PortfolioWeb/Pages/Professional.cshtml.cs
@@ -1,11 +1,22 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PortfolioWeb.Model;
+using PortfolioWeb.Services;
+using System.Collections.Generic;
 
 namespace PortfolioWeb.Pages
 {
     public class ProfessionalModel : PageModel
     {
+        public JsonFileSectionService SectionService;
+        public IEnumerable<Section> SectionList { get; private set; }
+
+        public ProfessionalModel(JsonFileSectionService sectionService)
+        {
+            SectionService = sectionService;
+        }
         public void OnGet()
         {
+            SectionList = SectionService.GetSections();
         }
     }
 }
