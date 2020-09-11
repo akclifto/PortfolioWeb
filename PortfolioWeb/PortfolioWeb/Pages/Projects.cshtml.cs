eef441cf0de0255cad1be63d1aa02f46087af1ba
akclifto <akclifto@asu.edu>
2020-09-11 11:23:46 -0700

    convert to poor mans db projects page

--- PortfolioWeb/PortfolioWeb/Pages/Projects.cshtml.cs
+++ PortfolioWeb/PortfolioWeb/Pages/Projects.cshtml.cs
@@ -1,11 +1,22 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PortfolioWeb.Model;
+using PortfolioWeb.Services;
+using System.Collections.Generic;
 
 namespace PortfolioWeb.Pages
 {
     public class ProjectsModel : PageModel
     {
+        public JsonFileSectionService SectionService;
+        public IEnumerable<Section> SectionList { get; private set; }
+
+        public ProjectsModel(JsonFileSectionService sectionService)
+        {
+            SectionService = sectionService;
+        }
         public void OnGet()
         {
+            SectionList = SectionService.GetSections();
         }
     }
 }
